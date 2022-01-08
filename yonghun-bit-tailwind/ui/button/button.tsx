5a3b3f3682abc74747cca210da1bb9c47d16f95a
yonghunj <rrallvv@gmail.com>
2022-01-08 04:52:55 -0500

    ADD button component

---
+++ yonghun-bit-tailwind/ui/button/button.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+
+export type ButtonProps = {
+  /**
+   * a node to be rendered in the special component.
+   */
+  children?: ReactNode;
+};
+
+export function Button({ children }: ButtonProps) {
+  return (
+    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      hello from Button
+    </button>
+  );
+}
